efc786dda93b5f235615b96dae3a1094c7fb97bb
khushalkothari <khushal.kothari04@gmail.com>
2021-06-23 17:25:22 +0530

    login

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -12,7 +12,7 @@ public class LoginPage {
 		
 		driver.get("https://en-gb.facebook.com/");
 		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rubykothari_96@yahoo.com");
-		driver.findElement(By.xpath("//*[@name='pass']")).sendKeys("Rub!9408125739");
+		driver.findElement(By.xpath("//*[@name='pass']")).sendKeys("Rub!");
 		driver.findElement(By.xpath("//*[@name='login']")).click();
 		
 		driver.close();
