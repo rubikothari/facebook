78dbbcd74a2e8264f3b8042a23e4f7baa12b75e2
khushalkothari <khushal.kothari04@gmail.com>
2021-07-01 17:11:57 +0530

    isDisplayed()

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LoginPage {
 
@@ -9,14 +10,28 @@ public class LoginPage {
 			
 		System.setProperty("webdriver.chrome.driver", "C:\\\\rubi\\\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		
+	
 		driver.get("https://en-gb.facebook.com/");
-		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rubykothari_96@yahoo.com");
-		driver.findElement(By.xpath("//*[@name='pass']")).sendKeys("Rub!");
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("aaa@yahoo.com");
+		driver.findElement(By.xpath("//*[@name='pass']")).sendKeys("password");
 		driver.findElement(By.xpath("//*[@name='login']")).click();
 		
-		driver.close();
+		if( driver.findElement(By.xpath("//*[@aria-label='Messenger']")).isDisplayed()){
+			System.out.println("Messenger is Visible");
+			}else{
+			System.out.println("Messenger is InVisible");
+			}
 		
-	}
-
+		if( driver.findElement(By.xpath("//*[@name='global_typeahead']")).isDisplayed()){
+			System.out.println("search is Visible");
+			}else{
+			System.out.println("search is InVisible");
+			}
+		if( driver.findElement(By.xpath("//*[@search is Visible='Home']")).isDisplayed()){
+			System.out.println("home is Visible");
+			}else{
+			System.out.println("home is InVisible");
+			}
+		driver.close();
+}
 }
